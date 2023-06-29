6518d12cfe4d20d77601e493373c984f72483018
MinhThuy206 <dothibichthuy_t65@hus.edu.vn>
2023-06-29 14:02:39 +0700

    commit

--- src/main/java/com/example/productmanagement/controllers/BatchController.java
+++ src/main/java/com/example/productmanagement/controllers/BatchController.java
@@ -1,4 +1,40 @@
 package com.example.productmanagement.controllers;
 
+import com.example.productmanagement.model.Batch;
+import com.example.productmanagement.service.BatchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/batch")
 public class BatchController {
+    @Autowired
+    private BatchService batchService;
+
+    @GetMapping("/")
+    public List<Batch> getAllBatch() {
+        return batchService.getAllBath();
+    }
+
+    @GetMapping("/getOneBatch")
+    public Batch getOneBatch(@RequestParam("id") long id) {
+        return batchService.getOneBatch(id);
+    }
+
+    @PostMapping("/")
+    public Batch addBatch(@RequestBody Batch batch) {
+        return batchService.addBatch(batch);
+    }
+
+    @PutMapping("/update")
+    public Batch updateBatch(@RequestParam("id") long id, @RequestBody Batch batch) {
+        return batchService.updateBatch(id, batch);
+    }
+
+    @DeleteMapping("/delete")
+    public boolean deleteBatch(@PathVariable("id") long id) {
+        return batchService.deleteBatch(id);
+    }
 }
--- src/main/java/com/example/productmanagement/controllers/ManuController.java
+++ src/main/java/com/example/productmanagement/controllers/ManuController.java
@@ -1,4 +1,45 @@
 package com.example.productmanagement.controllers;
 
+import com.example.productmanagement.model.Category;
+import com.example.productmanagement.model.Manufacturer;
+import com.example.productmanagement.service.ManuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/manu")
 public class ManuController {
+    @Autowired
+    private ManuService manuService;
+
+    @GetMapping("/")
+    public List<Manufacturer> getAllCategories() {
+        return manuService.getAllManu();
+    }
+
+    // API add product
+    @PostMapping("/")
+    public Manufacturer addManu(@RequestBody Manufacturer manu) {
+        return manuService.addManu(manu);
+    }
+
+    @GetMapping("/getOneManu")
+    public Manufacturer geManuDetail(@RequestParam("id") long id) {
+        return manuService.getOneManu(id);
+    }
+
+
+    //update Product
+    @PutMapping("/update")
+    public Manufacturer updateManu(@RequestParam("id") long id, @RequestBody Manufacturer manu) {
+        return manuService.updateManu(id, manu);
+    }
+
+    //    API delete product
+    @DeleteMapping("/{id}")
+    public boolean deletManu(@PathVariable("id") long id) {
+        return manuService.deleteManu(id);
+    }
 }
